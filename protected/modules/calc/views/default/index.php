7b7b018f8875e667b0d0774ec4842f19673cfba9
Vitaliy Kotov <mail@v-kotov.ru>
2012-09-18 19:01:38 +0600

    js linked

--- protected/modules/calc/views/default/index.php
+++ protected/modules/calc/views/default/index.php
@@ -2,9 +2,11 @@
 /* @var $this DefaultController */
 
 Yii::app()->clientScript->registerScriptFile(
-		   Yii::app()->baseUrl.'js/calc.js'
+		   Yii::app()->baseUrl.'/js/calc.js'
 		);
 
 ?>
 
 <h1>Calc here.</h1>
+<button type="button" onclick="helloDiletant();">Hello?</button>
+<br/><br/><br/>
