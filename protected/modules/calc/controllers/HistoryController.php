28b8c6b3fc65d639b3059576122d1e6eab915dca
Vitaliy Kotov <mail@v-kotov.ru>
2012-09-18 15:47:47 +0600

    simple JSON output

--- protected/modules/calc/controllers/HistoryController.php
+++ protected/modules/calc/controllers/HistoryController.php
@@ -4,16 +4,26 @@ class HistoryController extends Controller
 {
 	public function actionIndex()
 	{
+		header('Content-type: application/json');
+		
 		$history = array(
 					'5+4=9',
 					'6*8=48',
 				);
+		
 		echo CJSON::encode($history);
+		
+		Yii::app()->end();
 	}
 	
 	public function actionAdd()
 	{
+		header('Content-type: application/json');
+		
 		$result = 'Ok';
+		
 		echo CJSON::encode($result);
+		
+		Yii::app()->end();
 	}
 }
