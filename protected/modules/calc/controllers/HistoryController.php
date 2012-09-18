fed89bf3620ef731e50a7f1b3202c13da7a61056
Vitaliy Kotov <mail@v-kotov.ru>
2012-09-18 13:43:50 +0600

    history controller created

---
+++ protected/modules/calc/controllers/HistoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+class HistoryController extends Controller
+{
+	public function actionIndex()
+	{
+		$history = array(
+					'5+4=9',
+					'6*8=48',
+				);
+		echo CJSON::encode($history);
+	}
+	
+	public function actionAdd()
+	{
+		$result = 'Ok';
+		echo CJSON::encode($result);
+	}
+}
