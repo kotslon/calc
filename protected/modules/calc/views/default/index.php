6dc38fa633ddd7fd66ac69c47e67a7493b34c2c1
Vladimir Shalaev <pharaon.vvs@gmail.com>
2012-09-20 14:22:33 +0600

    styles fixes

--- protected/modules/calc/views/default/index.php
+++ protected/modules/calc/views/default/index.php
@@ -6,7 +6,7 @@ Yii::app()->clientScript->registerCssFile(
 );
 
 Yii::app()->clientScript->registerCssFile(
-		Yii::app()->baseUrl.'/css/calc_style.css'
+		Yii::app()->baseUrl.'/css/calc.css'
 );
 
 Yii::app()->clientScript->registerScriptFile(
@@ -25,140 +25,53 @@ Yii::app()->clientScript->registerScriptFile(
 	<script src="js/bootstrap.js"></script>
 <!--  style="padding-left:130px"-->
 
-
-	<div class="conteiner span12 offset2" style="margin: 0px; margin-top: 15px; padding: 0px;" >
-		<div class="row-fluid" style="margin: 0px; padding: 0px" >
-			<div class="span8 " style="margin: 0px; padding: 0px">
-				<div class="input-append" style="margin: 0px; padding: 0px">
-					<input class="span8" readonly id="appendedInputButton" style="width: 88%; background-color: #ffffff" size= "116" type="text">
-						<button class="btn btn-warning" type="button" id="ButtonTwit" onClick="calc.pressButton('Twitter')">Twitter</button>
-					
-					<div class="conteiner-fluid" style="margin: 0px; padding: 0px">
-							<div class="span12" style="margin: 0px; padding: 0px">
-								<div class="span3" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="ButtonC" onClick="calc.pressButton('ButtonC')" style="width: 100%; line-height: 40px">C</button></div>
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px;"><h5 style="text-align: center">Diletant Club<br/> web development by newbies</h5></div>
-								<div class="span3" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="ButtonBcsp" onClick="calc.pressButton('ButtonBcsp')" style="width: 100%; line-height: 40px">Bck</button></div>
-							</div>
-														
-							<div class=" span8" style="margin: 0px; padding: 0px">
-								<div class=" span4" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button7" onClick="calc.pressButton('Button7')" style="width: 100%; line-height: 45px;">7</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button8" onClick="calc.pressButton('Button8')" style="width: 100%; line-height: 45px;">8</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button9" onClick="calc.pressButton('Button9')" style="width: 100%; line-height: 45px;">9</button></div>
-																
-								<div class=" span4" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button4" onClick="calc.pressButton('Button4')" style="width: 100%; line-height: 45px;">4</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button5" onClick="calc.pressButton('Button5')" style="width: 100%; line-height: 45px;">5</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button6" onClick="calc.pressButton('Button6')" style="width: 100%; line-height: 45px;">6</button></div>
-								
-								<div class=" span4" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button1" onClick="calc.pressButton('Button1')" style="width: 100%; line-height: 45px;">1</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button2" onClick="calc.pressButton('Button2')" style="width: 100%; line-height: 45px;">2</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button3" onClick="calc.pressButton('Button3')" style="width: 100%; line-height: 45px;">3</button></div>
-								
-								<div class=" span4" style="margin: 0px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button0" onClick="calc.pressButton('Button0')" style="width: 100%; line-height: 45px;">0</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="Button00" onClick="calc.pressButton('Button00')" style="width: 100%; line-height: 45px;">00</button></div>
-								<div class=" span4" style="margin-left: 5px; padding: 0px; margin-top: 5px;"><button class="btn" id="ButtonDot" onClick="calc.pressButton('ButtonDot')" style="width: 100%; line-height: 45px;">.</button></div>	
-							</div>
-							<div class="span4" style="margin: 0px; padding: 0px; width: 210px">
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 0px"><button class="btn" id="ButtonMinus" onClick="calc.pressButton('ButtonMinus')" style="width: 100%; line-height: 45px">-</button></div>
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 4px"><button class="btn" id="ButtonNeg" onClick="calc.pressButton('ButtonNeg')" style="width: 100%; line-height: 45px;">-/+</button></div>
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 0px"><button class="btn" id="ButtonMul" onClick="calc.pressButton('ButtonMul')" style="width: 100%; line-height: 45px;">*</button></div>
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 4px"><button class="btn" id="ButtonDiv" onClick="calc.pressButton('ButtonDiv')" style="width: 100%; line-height: 45px;">/</button></div>		
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 0px"><button class="btn" id="ButtonPlus" onClick="calc.pressButton('ButtonPlus')" style="width: 100%; line-height: 105px;">+</button></div>
-								<div class="span6" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 4px"><button class="btn" id="ButtonEq" onClick="calc.pressButton('ButtonEq')" style="width: 100%; line-height: 105px;">=</button></div>
-							</div>				
-							
-					</div>
-					
-						
-<!-- 			<table class="table-boardered" style="width: 100%; border-spacing: 0;" >
-						<thead>
-							<tr>	
-								<th class="well" ><button class="btn">C</button></th>
-								<th class="well" colspan="3" >""</th>
-								<th class="well" ><button class="btn">Bcsp</button></th>
-							</tr>	
-						</thead>
-						<tbody>
-							<tr  >
-								<td><button class="btn" Style="width: 80%; padding: 10%">7</button></td>
-								<td><button class="btn" >8</button></td>
-								<td><button class="btn">9</button></td>
-								<td><button class="btn">-</button></td>
-								<td><button class="btn">-/+</button></td>
-							</tr>
-							<tr>
-								<td><button class="btn">4</button></td>
-								<td><button class="btn">5</button></td>
-								<td><button class="btn">6</button></td>
-								<td><button class="btn">*</button></td>
-								<td><button class="btn">/</button></td>
-							</tr>
-							<tr>
-								<td><button class="btn">1</button></td>
-								<td><button class="btn">2</button></td>
-								<td><button class="btn">3</button></td>
-								<td rowspan="2"><button class="btn" style="line-height: 47px; width: 57px">+</button></td>
-								<td rowspan="2"><button class="btn btn-primary" style="line-height: 47px; line-width: 47px">=</button></td>
-								
-							</tr>
-							<tr>
-								<td><button class="btn">0</button></td>
-								<td><button class="btn">00</button></td>
-								<td><button class="btn">.</button></td>
-								
-							</tr>						
-						
-						</tbody>
-					
-					
-					</table>  -->
-				
-				 </div>
-			</div>
-			<div class="span4">
-				<div class="input-append">
-					<textarea class="span12" rows="14" readonly style="background-color: #ffffff; resize: none"></textarea>
-				</div>
-			</div>
-			
-		</div>
-
-		<footer class="footer" style="background-color:#ffffff; margin-top: 20%; padding: 0px">
-		    <div class="container" style="margin:0 auto;">
-		       <!--  <div class="row">
-		            <div class="well span2 offset1" style="font-size: 10px">
-		                <h5>Company</h5>
-		                <ul>
-		                    <li><a href="">About Us</a></li>
-		                    <li><a href="">Contact</a></li>
-		                </ul>
-		            </div>
-		            <div class="well span2 offset1" style="font-size: 10px">
-		                <h5>Resource</h5>
-		                <ul>
-		                    <li style="padding-top:110"><a href="">Get help</a></li>
-		                    <li><a href="">FAQ</a></li>
-		                    <li><a href="">Support</a></li>
-		                </ul>
-		            </div>
-		            <div class="well span2 offset1" style="font-size: 10px">
-		                <h5>Connect</h5>
-		                <ul>
-		                    <li><a href="" title="Facebook">Facebook</a></li>
-		                    <li><a href="" title="Twitter">Twitter</a></li>
-		                    <li><a href="" title="LinkedIn">LinkedIn</a></li>
-		                    <li><a href="" title="Google+">Google+</a></li>
-		                    <li><a href="" title="RSS">RSS</a></li>
-		                    <li><a href="" title="Blogs">Blogs</a></li>
-		                </ul>
-		            </div>
-		         </div> -->
-		        <div class="well navbar span12 " style="margin: 0px; padding: 0px">
-
-		        <h6 style="margin-left: 25%">Copyright &copy; somewhen by Diletant club. All Rights Reserved. Powered by Yii Framework. </h6>
-
-		        </div>
-		    </div>
-		</footer>
-		<button type="button" onclick="calc.loadHistory();">Load history</button>
-		<button type="button" onclick="calc.addToHistory();">Add to history</button>
+		
+<div class="span10">
+	<div class="input-append">
+		<input class="span10" readonly id="appendedInputButton" style="background-color: #ffffff" size= "16" type="text">
+		<button class="btn btn-warning" type="button" id="ButtonTwit" onClick="calc.pressButton('Twitter')">Twitter</button>
 	</div>	
+	<div class="row">
+		<div class="span2"><button class="btn" id="ButtonC" onClick="calc.pressButton('ButtonC')" style="width: 100%; line-height: 40px">C</button></div>
+		<div class="span6"><h5 style="text-align: center">Diletant Club<br/> web development by newbies</h5></div>
+		<div class="span2"><button class="btn" id="ButtonBcsp" onClick="calc.pressButton('ButtonBcsp')" style="width: 100%; line-height: 40px">Bck</button></div>
+	</div>										
+	<div class="row">
+		<div class="span2"><button class="btn" id="Button7" onClick="calc.pressButton('Button7')" style="width: 100%; line-height: 45px;">7</button></div>
+		<div class="span2" ><button class="btn" id="Button8" onClick="calc.pressButton('Button8')" style="width: 100%; line-height: 45px;">8</button></div>
+		<div class="span2" ><button class="btn" id="Button9" onClick="calc.pressButton('Button9')" style="width: 100%; line-height: 45px;">9</button></div>
+		<div class="span2"><button class="btn" id="ButtonMinus" onClick="calc.pressButton('ButtonMinus')" style="width: 100%; line-height: 45px">-</button></div>
+		<div class="span2"><button class="btn" id="ButtonNeg" onClick="calc.pressButton('ButtonNeg')" style="width: 100%; line-height: 45px;">-/+</button></div>
+	</div>									
+	<div class ="row">				
+					
+		<div class="span2"><button class="btn" id="Button4" onClick="calc.pressButton('Button4')" style="width: 100%; line-height: 45px;">4</button></div>
+		<div class="span2" ><button class="btn" id="Button5" onClick="calc.pressButton('Button5')" style="width: 100%; line-height: 45px;">5</button></div>
+		<div class="span2" ><button class="btn" id="Button6" onClick="calc.pressButton('Button6')" style="width: 100%; line-height: 45px;">6</button></div>
+		<div class="span2" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 0px"><button class="btn" id="ButtonMul" onClick="calc.pressButton('ButtonMul')" style="width: 100%; line-height: 45px;">*</button></div>
+		<div class="span2" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 4px"><button class="btn" id="ButtonDiv" onClick="calc.pressButton('ButtonDiv')" style="width: 100%; line-height: 45px;">/</button></div>
+	</div>			
+	<div class="row span6">				
+		<div class="row">
+			<div class=" span2"><button class="btn" id="Button1" onClick="calc.pressButton('Button1')" style="width: 100%; line-height: 45px;">1</button></div>
+			<div class=" span2" ><button class="btn" id="Button2" onClick="calc.pressButton('Button2')" style="width: 100%; line-height: 45px;">2</button></div>
+			<div class=" span2" ><button class="btn" id="Button3" onClick="calc.pressButton('Button3')" style="width: 100%; line-height: 45px;">3</button></div>
+		</div>
+		<div class="row">
+			<div class=" span2"><button class="btn" id="Button0" onClick="calc.pressButton('Button0')" style="width: 100%; line-height: 45px;">0</button></div>
+			<div class=" span2" ><button class="btn" id="Button00" onClick="calc.pressButton('Button00')" style="width: 100%; line-height: 45px;">00</button></div>
+			<div class=" span2" ><button class="btn" id="ButtonDot" onClick="calc.pressButton('ButtonDot')" style="width: 100%; line-height: 45px;">.</button></div>
+		</div>
+	</div>				
+	<div class="span4" style="margin: 0px; padding: 0px; width: 210px">
+		<div class="span2" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 0px"><button class="btn" id="ButtonPlus" onClick="calc.pressButton('ButtonPlus')" style="width: 100%; line-height: 105px;">+</button></div>
+		<div class="span2" style="margin: 0px; padding: 0px; margin-top: 5px; margin-left: 4px"><button class="btn" id="ButtonEq" onClick="calc.pressButton('ButtonEq')" style="width: 100%; line-height: 105px;">=</button></div>
+	</div>							
+</div>
+<div class="span2">
+	<div class="input-append">
+		<textarea class="span2" rows="14" readonly style="background-color: #ffffff; resize: none"></textarea>
+	</div>
+</div>
+			
+	
