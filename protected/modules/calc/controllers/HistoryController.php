faeb564ef7a79da19d29694182685e61ac17d72d
Vitaliy Kotov <mail@v-kotov.ru>
2012-09-20 14:42:39 +0600

    save/read to/from db

--- protected/modules/calc/controllers/HistoryController.php
+++ protected/modules/calc/controllers/HistoryController.php
@@ -3,24 +3,27 @@
 class HistoryController extends Controller
 {
 	public function actionIndex()
-	{
+	{		
+		$hist = new History;
+		//$history = array('5+4=9', '6*8=48');
+		$history = History::model()->findAll();
+		
 		header('Content-type: application/json');
-		
-		$history = array('5+4=9', '6*8=48');
-		
 		echo CJSON::encode($history);
-		
 		Yii::app()->end();
 	}
 	
-	public function actionAdd()
+	public function actionAdd($e)
 	{
+		$expr = (string) $e;
+		
+		$hist = new History;
+		$hist->text = $expr;
+		$hist->save();
+		
 		header('Content-type: application/json');
-		
-		$result = 'Ok';
-		
+		$result = $expr;
 		echo CJSON::encode($result);
-		
 		Yii::app()->end();
 	}
 }
