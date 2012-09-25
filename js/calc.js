a20b36ea1561ca6398b4cef3b7bc88ceac8fe3bd
Sergey Gonimar <sergey.gonimar@gmail.com>
2012-09-25 21:47:52 +0600

    Calc actions

--- js/calc.js
+++ js/calc.js
@@ -2,6 +2,11 @@
 function Calc (hgl, hal) {
 	var history = [];
 	var lastCalc = "4-1=3";
+
+	var expr = "";
+	var log = "";
+	var flag = false;
+	
 	this.history_get_link = hgl;
 	this.history_add_link = hal;
 }
@@ -24,7 +29,7 @@ Calc.prototype.loadHistory = function () {
 		success: function (result) {
 			history = result;
 			this.showHistory();
-		},
+		}
 	});
 }
 
@@ -38,13 +43,82 @@ Calc.prototype.addToHistory = function () {
 		success: function (result) {
 			console.log(result);
 			this.loadHistory();
-		},
+		}
 	});
 }
 
 Calc.prototype.pressButton = function (x) {
-	
 		alert(x);
+}
+
+Calc.prototype.Operand = function (x) {
+	if(this.flag) {
+		this.expr = "";
+		this.flag = false;
+	}
+	if(!this.expr) {
+		this.expr = "";
+	}
+	if(!this.log) {
+		this.log = "";
+	}
+	this.expr += x;
+	this.log += x;
+	$('#expr').html(this.expr);
+	$('#log').html(this.log);
+	if(this.log) {
+		$('#log').show();
+	}
+}
+
+Calc.prototype.Operator = function (x) {
+	if(this.expr && !this.flag) {
+		this.log = eval(this.log);
+		this.log += x;
+		this.flag = true;
+		$('#expr').html(this.expr);
+		$('#log').html(this.log);
+		if(this.log) {
+			$('#log').show();
+		}
+	}
+}
+Calc.prototype.Equal = function () {
+	if(this.expr && !this.flag) {
+		this.expr = eval(this.log);
+		this.log = this.expr;
+		//this.flag = true;
+		$('#expr').html(this.expr);
+		$('#log').hide();
+	}
+}
+
+Calc.prototype.Back = function () {
+	if(this.expr) {
+		this.expr = this.expr.toString().substr(0,this.expr.toString().length-1);
+		this.log = this.expr;
+		//this.flag = true;
+		$('#expr').html(this.expr);
+		$('#log').hide();
+	}
+}
+
+Calc.prototype.Clear = function () {
+	this.expr = "";
+	this.log = "";
+	this.flag = false;
+	$('#expr').html(0);
+	$('#expr').html("");
+	$('#log').hide();
+}
+
+Calc.prototype.Neg = function () {
+	this.expr = "";
+	this.log = "";
+	this.flag = false;
+	$('#expr').html(0);
+	$('#expr').html("Stack overflow!");
+	$('#log').hide();
 }
 
 var calc;
--- protected/modules/calc/views/default/index.php
+++ protected/modules/calc/views/default/index.php
@@ -5,43 +5,50 @@
 		<div class="input-append">
 			<input class="span7" readonly id="appendedInputButton" style="background-color: #ffffff"  type="text">
 			<button class="btn btn-warning" type="button" id="ButtonTwit" onClick="calc.pressButton('Twitter')">Twitter</button>
-		</div>	
+		</div>
+		
+		<div id="calc-disp" class="well" style="text-align:right;">
+			<small id="log" style="display: none; padding-top: -10px;"></small>
+			<hr style="padding: 0px; margin: 0px;" />
+			<strong id="expr">0</strong>
+		</div>
+		
 		<div class="row">
-			<div class="span2"><button class="btn btn-info calcBtn" id="ButtonC" onClick="calc.pressButton('ButtonC')">C</button></div>
+			<div class="span2"><button class="btn btn-info calcBtn" id="ButtonC" onClick="calc.Clear()">C</button></div>
 			<div class="span4"><img src="images/logo_DC.png" class="img-rounded"></div>
-			<div class="span2"><button class="btn btn-info calcBtn" id="ButtonBcsp" onClick="calc.pressButton('ButtonBcsp')">Bck</button></div>
+			<div class="span2"><button class="btn btn-info calcBtn" id="ButtonBcsp" onClick="calc.Back()">Bck</button></div>
 		</div>										
 		<div class="row">
-			<div class="span2"><button class="btn calcBtn" id="Button7" onClick="calc.pressButton('Button7')">7</button></div>
-			<div class="span2"><button class="btn calcBtn" id="Button8" onClick="calc.pressButton('Button8')">8</button></div>
-			<div class="span2"><button class="btn calcBtn" id="Button9" onClick="calc.pressButton('Button9')">9</button></div>
-			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonMinus" onClick="calc.pressButton('ButtonMinus')">-</button></div>
-			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonNeg" onClick="calc.pressButton('ButtonNeg')">-/+</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button7" onClick="calc.Operand('7')">7</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button8" onClick="calc.Operand('8')">8</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button9" onClick="calc.Operand('9')">9</button></div>
+			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonMinus" onClick="calc.Operator('-')">-</button></div>
+			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonNeg" onClick="calc.Neg()">-/+</button></div>
 		</div>									
 		<div class ="row">				
-			<div class="span2"><button class="btn calcBtn" id="Button4" onClick="calc.pressButton('Button4')" >4</button></div>
-			<div class="span2"><button class="btn calcBtn" id="Button5" onClick="calc.pressButton('Button5')" >5</button></div>
-			<div class="span2"><button class="btn calcBtn" id="Button6" onClick="calc.pressButton('Button6')" >6</button></div>
-			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonMul" onClick="calc.pressButton('ButtonMul')">*</button></div>
-			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonDiv" onClick="calc.pressButton('ButtonDiv')">/</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button4" onClick="calc.Operand('4')" >4</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button5" onClick="calc.Operand('5')" >5</button></div>
+			<div class="span2"><button class="btn calcBtn" id="Button6" onClick="calc.Operand('6')" >6</button></div>
+			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonMul" onClick="calc.Operator('*')">*</button></div>
+			<div class="span1"><button class="btn btn-info calcBtn" id="ButtonDiv" onClick="calc.Operator('/')">/</button></div>
 		</div>			
 		<div class="row">	
 			<div class="span6">				
 				<div class="row">
-					<div class=" span2"><button class="btn calcBtn" id="Button1" onClick="calc.pressButton('Button1')">1</button></div>
-					<div class=" span2"><button class="btn calcBtn" id="Button2" onClick="calc.pressButton('Button2')">2</button></div>
-					<div class=" span2"><button class="btn calcBtn" id="Button3" onClick="calc.pressButton('Button3')">3</button></div>
+					<div class=" span2"><button class="btn calcBtn" id="Button1" onClick="calc.Operand('1')">1</button></div>
+					<div class=" span2"><button class="btn calcBtn" id="Button2" onClick="calc.Operand('2')">2</button></div>
+					<div class=" span2"><button class="btn calcBtn" id="Button3" onClick="calc.Operand('3')">3</button></div>
 				</div>
 				<div class="row">
-					<div class=" span2"><button class="btn calcBtn" id="Button0" onClick="calc.pressButton('Button0')">0</button></div>
+					<div class=" span2"><button class="btn calcBtn" id="Button0" onClick="calc.Operand('0')">0</button></div>
 					<div class=" span2"><button class="btn calcBtn" id="Button00" onClick="calc.pressButton('Button00')">00</button></div>
 					<div class=" span2"><button class="btn calcBtn" id="ButtonDot" onClick="calc.pressButton('ButtonDot')">.</button></div>
 				</div>
 			</div>				
 			<div class="span2">
 				<div class="row">	
-					<div class="span1"><button class="btn btn-info calcBtn bigBtn" id="ButtonPlus" onClick="calc.pressButton('ButtonPlus')">+</button></div>
-					<div class="span1"><button class="btn btn-info calcBtn bigBtn" id="ButtonEq" onClick="calc.pressButton('ButtonEq')">=</button></div>
+					<div class="span1"><button class="btn btn-info calcBtn bigBtn" id="ButtonPlus" onClick="calc.Operator('+')">+</button></div>
+					<div class="span1"><button class="btn btn-info calcBtn bigBtn" id="ButtonEq" onClick="calc.Equal()">=</button></div>
 				</div>
 			</div>
 		</div>								
