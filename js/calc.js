b9733af2a10500d1c7b65711ba0da8e6ec23e991
Vitaliy Kotov <mail@v-kotov.ru>
2012-09-26 14:59:38 +0600

    operand editing fixes

--- js/calc.js
+++ js/calc.js
@@ -14,6 +14,8 @@ function Calc(hgl,hal) {
 	var BTNS_OP = ['btnPlus','btnMinus','btnMul','btnDiv'];  // buttons for operations
 	var BTNS_ED = ['btn0','btn1','btn2','btn3','btn4','btn5','btn6','btn7','btn8','btn9',
 	               'btn00','btnDot','btnBcsp','btnNeg'];  // buttons for editing
+	var MAX_DIGITS = 20;
+	var MAX_DEC = 8;
 	
 	function Operand() {
 		var _string = '0';
@@ -27,7 +29,12 @@ function Calc(hgl,hal) {
 		}
 		
 		this.setValue = function (v) {
+			var p = Math.pow(10,MAX_DEC);
+			LOG.s('setting value: '+v);
+			v = Math.round(v*p)/p;
+			LOG.s('rounded value: '+v);
 			_string = v.toString();
+			this.edit('none');
 		}
 		
 		this.edit = function (btn) {
@@ -44,16 +51,39 @@ function Calc(hgl,hal) {
 			case 'btn8': _string+='8'; break;
 			case 'btn9': _string+='9'; break;
 			case 'btnDot': if($.inArray('.',_string)===-1){_string+='.';} break;
-			case 'btnNeg': _string='-'+_string; break;
+			case 'btnNeg': 
+				if($.inArray('-',_string)===-1){
+					_string='-'+_string; 
+				} else {_
+					string=_string.substring(1);
+				}
+			break;
 			case 'btnBcsp':  _string = _string.substring(0,_string.length-1); break;
 			};
+			// save sign
 			var sign = (_string[0]==='-'?'-':'+');
+			// cut sign out
 			if (sign==='-'){_string=_string.substring(1);}
+			// cut leading zeros
 			while(_string[0]==='0'){_string=_string.substring(1);}
+			// add leading zero for values with abs. less than 1
+			if (_string[0]==='.'){_string = '0'+_string;}
 			if (_string.length===0){
 				_string='0';
-			} else if (sign==='-'){
+			} else if (sign==='-'){ // put sign back
 				_string=sign+_string;
+			}
+			// cut digits after decimal point
+			var point = $.inArray('.',_string);
+			if(point>=0){
+				var dec_s = _string.substring(point+1); 
+				if (dec_s.length > MAX_DEC){
+					_string = _string.substring(0,point+1) + dec_s.substring(0,MAX_DEC);					
+				}
+			}
+			// cut digits > MAX_DIGITS
+			if(_string.length > MAX_DIGITS){
+				_string = _string.substring(0,MAX_DIGITS);
 			}
 		}
 	}
@@ -127,10 +157,12 @@ function Calc(hgl,hal) {
 		case 'mul': res = op1.getValue()*op2.getValue(); op_s = '*';break;
 		case 'div': res = op1.getValue()/op2.getValue(); op_s = '/';break;
 		}
-		var res_s = op1.getString()+' '+op_s+' '+op2.getString()+' = '+res;
+		var res_s = op1.getString()+' '+op_s+' '+op2.getString()+' = ';
 		reset();
 		op1.setValue(res);
 		out(op1);
+		res_s += op1.getString();
+		LOG.s('res === '+res);
 		LOG.s('<<< '+res_s);
 		addToHistory(res_s);
 	}
